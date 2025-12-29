71a029e1923eeeedc4ecc3600fafb5033e1da60c
MohitD9 <mohiit99999@gmail.com>
2025-12-29 19:38:49 +0530

    Update SQL Code_2.sql

--- SQL Code_2.sql
+++ SQL Code_2.sql
@@ -4,3 +4,12 @@ SELECT
 FROM market_prices_large
 GROUP BY FORMAT(CAST([date] AS date), 'yyyy-MM')
 ORDER BY month;
+
+
+month	avg_monthly_price
+2023-10	82.71
+2023-11	78.49
+2023-12	75.65
+2024-01	76.72
+2024-02	75.63
+2024-03	76.09
